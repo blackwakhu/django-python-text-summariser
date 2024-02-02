40abb0e999d0c345ca905ff3a90afc9269479d57
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-02-02 14:12:48 +0300

    summariser added

--- text/tasks.py
+++ text/tasks.py
@@ -1,1 +1,14 @@
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+import nltk 
+
+nltk.download('punkt')
+
+
+def summarize(text, language="english", sentences_count=5):
+  parser = PlaintextParser.from_string(text, Tokenizer(language))
+  summarizer = LsaSummarizer()
+  summary = summarizer(parser.document, sentences_count)
+  return ' '.join(str(sentence) for sentence in summary)
 
