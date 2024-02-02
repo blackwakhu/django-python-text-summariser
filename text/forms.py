b3018923e20840b62a8758cb29994393985d5d64
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-02-02 15:33:29 +0300

    Create forms.py

---
+++ text/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class SummaryForm(forms.Form):
+  inptext = forms.CharField(label="Enter text to summarise", widget=forms.Textarea(attrs={'rows': '10'}))
+  count = forms.IntegerField(label="Enter number of sentences to summarise", widget=forms.NumberInput(attrs={'min':'3'}))
