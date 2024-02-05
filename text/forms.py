c90880e73e6fb6e16fadf9e1d9d3782e4fbdaba9
Derrick wakhu shibero <98383475+blackwakhu@users.noreply.github.com>
2024-02-05 14:45:56 +0300

    added the url forms

--- text/forms.py
+++ text/forms.py
@@ -4,3 +4,7 @@ from django import forms
 class SummaryForm(forms.Form):
   inptext = forms.CharField(label="Enter text to summarise", widget=forms.Textarea(attrs={'rows': '10'}))
   count = forms.IntegerField(label="Enter number of sentences to summarise", widget=forms.NumberInput(attrs={'min':'3'}))
+
+class UrlSummaryForm(forms.Form):
+  inptext = forms.CharField(label="enter the url")
+  count = forms.IntegerField(label="Enter number of sentences to summarise", widget=forms.NumberInput(attrs={'min':'3'}))
